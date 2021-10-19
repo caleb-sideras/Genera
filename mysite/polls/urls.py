d55254c4de0e57943236fd43fcefad38bd1e8c03
Caleb Sideras <CJSideras@student.fullsail.edu>
2021-10-19 15:06:34 -0400

    testing djangos mother

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,11 +3,14 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from polls.views import main_view
+from polls.views import temp_view
+from polls.views import upload
 
 urlpatterns = [
 
     # include other apps
     path('', main_view, name="main_view"),
-    # path('', main_view, name="main_view"),
+    path('temp', temp_view, name="temp_view"),
+    path('upload', upload, name="upload")
 
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from polls.generator_alg import *
 from django.templatetags.static import static
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 
@@ -15,3 +16,25 @@ def main_view(request):
     context = {}
 
     return render(request, 'home.html', context={"created": created})
+
+
+def temp_view(request):
+
+    # if request.METHOD == "POST":
+
+    # created = createImage
+
+    # x = generateRandomNumber(1, 3, 5)
+    # context = {}
+
+    return render(request, 'testerman.html')
+
+
+def upload(request):
+    if request.method == 'POST' and request.FILES['upload']:
+        upload = request.FILES['upload']
+        fss = FileSystemStorage()
+        file = fss.save(upload.name, upload)
+        file_url = fss.url(file)
+        return render(request, 'upload.html', {'file_url': file_url})
+    return render(request, 'upload.html')
