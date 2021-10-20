ee664711060698bc297eaf8b694949ce56b40e02
Artem Grechushkin <2381169g@student.gla.ac.uk>
2021-10-20 19:34:23 +0100

    image generator + storing now the outputs

--- mysite/polls/generator_alg.py
+++ mysite/polls/generator_alg.py
@@ -1,6 +1,8 @@
 from PIL import Image
 import numpy as np
 import time
+import io
+import random
 
 # Seed for our random number generator, based on time
 
@@ -18,72 +20,39 @@ def generateRandomNumber(lowIn, highIn, sizeIn):
 # - the current textures Samoshin sent me are buggy, they only use A (Alpha) values in the RGBA format. He will fix
 # - the whole texture mapping process can be put into a function so any asset can have a texture put on it (maybe you can do this :DD)
 # - good luck :) Caleb in yr13 would have no idea how this works!!!
-
-
-def createImage():
-
-    # returns a 6 length array of random numbers ranging from 1 - 5
+    
+def createImage(files=[], n_textures=1):
     rnd = generateRandomNumber(1, 5, 6)
-
-    # opening assets based on their random number e.g. /Assets/Body/1.png
-    # (randomNumber % 4) + 1 = number of range from 1–4. each assets random number range is limited
     body = Image.open("static/Assets/" + "Body/" +
-                      str((rnd[0] % 1) + 1) + ".png")
-    shirt = Image.open("static/Assets/" + "Shirt/" +
-                       str((rnd[1] % 2) + 1) + ".png")
-    hair = Image.open("static/Assets/" + "Hair/" +
-                      str((rnd[2] % 2) + 1) + ".png")
-    accessories = Image.open("static/Assets/" + "Accessories/" +
-                             str((rnd[3] % 1) + 1) + ".png")
-    texture = Image.open("static/Assets/" + "Texture/" +
-                         str((rnd[4] % 4) + 1) + ".png")
+                        str((rnd[0] % 1) + 1) + ".png")
+    if files:
+        textures_array = files[:n_textures]
+        assets_array = files[n_textures:]
+        print(len(textures_array))
+        print(len(assets_array))
 
-    # only applying texture to hair and shirt (for now)
-    shirt_rgba = shirt.convert('RGBA')
-    hair_rgba = hair.convert('RGBA')
-    texture_rgba = texture.convert('RGBA')
+        for img in assets_array:
 
-    # converting objects into a numpy array, height x width x 4 numpy array (4000x4000x4)
-    shirt_data = np.array(shirt_rgba)
-    hair_data = np.array(hair_rgba)
-    texture_data = np.array(texture_rgba)
+            texture_rgba = random.choice(textures_array).convert('RGBA')
+            texture_data = np.array(texture_rgba)
 
-    # temporarily unpack the color bands for readability
-    red, green, blue, alpha = shirt_data.T
-    red2, green2, blue2, alpha2 = hair_data.T
+            temp_rgba = img.convert('RGBA')
+            temp_data = np.array(temp_rgba)
 
-    # extracting the area that is white from assets. creates an array of boolean, True = white point, False = not a white point (leaving alpha values alone for now)
-    shirt_white_areas = (red == 255) & (blue == 255) & (green == 255)
-    hair_white_areas = (red2 == 255) & (blue2 == 255) & (green2 == 255)
+            red, green, blue, alpha = temp_data.T
+            temp_white_areas = (red == 255) & (blue == 255) & (green == 255)
 
-    # using the white area arrays from above, points that are True are taken out of the texture array
-    texture_shirt_white_area = texture_data[...][shirt_white_areas.T].shape
-    texture_hair_white_area = texture_data[...][hair_white_areas.T].shape
+            texture_asset_white_area = texture_data[...][temp_white_areas.T].shape
 
-    # resizes the texture to the same shape of the asset, so they can be assigned to each other
-    texture_shirt_white_areas_resized = np.resize(
-        texture_data[...][shirt_white_areas.T], texture_shirt_white_area)
-    texture_hair_white_areas_resized = np.resize(
-        texture_data[...][hair_white_areas.T], texture_hair_white_area)
+            texture_asset_white_areas_resized = np.resize(
+            texture_data[...][temp_white_areas.T], texture_asset_white_area)
 
-    # replacing all the white areas in the asset with the texture
-    shirt_data[...][shirt_white_areas.T] = texture_shirt_white_areas_resized
-    hair_data[...][hair_white_areas.T] = texture_hair_white_areas_resized
+            temp_data[...][temp_white_areas.T] = texture_asset_white_areas_resized
 
-    # converting them back into an image
-    shirt = Image.fromarray(shirt_data)
-    hair = Image.fromarray(hair_data)
+            temp = Image.fromarray(temp_data)
+            body.paste(temp, (0, 0), temp)
+            # body.save("media/user_assets/BOSSMAN.png", "PNG")
 
-    # combining all assets to body
-    body.paste(shirt, (0, 0), shirt)
-    body.paste(hair, (0, 0), hair)
-    body.paste(accessories, (0, 0), accessories)
-
-    # shirt.show()
-    # hair.show()
-    # body.show()
-
-    # if you wanted to save
-    body.save("media/user_assets/1.png", "PNG")
-
-    return "success"
+        return body
+    else:
+        print("NO FILES LMAO")
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from mysite.settings import MEDIA_DIR
 from polls.generator_alg import *
 from django.templatetags.static import static
 from django.core.files.storage import FileSystemStorage
@@ -7,6 +8,11 @@ from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from pathlib import Path
+import base64
+from PIL import Image
+import numpy as np
+import io
 from .forms import *
 from .models import *
 
@@ -36,22 +42,43 @@ def temp_view(request):
 
     return render(request, 'testerman.html')
 
+def get_or_create_subdirectory(sd):
+    Path(f"{MEDIA_DIR}/{sd}").mkdir(parents=True, exist_ok=True)
 
 def upload_view(request):
     users_imgs = UserAsset.objects.filter(user=request.user)
 
-    if request.method == 'POST' and request.FILES['upload']:
-        upload = request.FILES['upload']
+    if request.method == 'POST':
+        if len(request.FILES) != 0:
+            files_array = []
+            n_textures = 0
+
+            for filename, file in request.FILES.items():
+                
+                file_obj = file.read()
+                image = Image.open(io.BytesIO(file_obj))
+                if filename.startswith("texture"):
+                    files_array.insert(0, image)
+                    n_textures += 1
+                else:
+                    files_array.append(image)
+                
+            generated_img = createImage(files_array, n_textures)
+        
+        file_dir = f"user_asset_storage/{request.user.username}"
+        get_or_create_subdirectory(file_dir)
+        filename = uuid.uuid4().hex[:5]
+        generated_img.save(f"media/{file_dir}/{filename}.png")
 
         asset_db = UserAsset.objects.create(user=request.user)
         asset_db.name = request.POST["name"]
-        asset_db.image = upload
+        asset_db.image = f"{file_dir}/{filename}.png"
         asset_db.save()
         users_imgs = UserAsset.objects.filter(user=request.user)
         
         print("SAVED TO DB")
+        messages.success(request, message="YOU HAVE GENERATED THE IMAGE IDIOTTERMAN.... ITS IN DB !!!!")
         return render(request, 'upload.html', {'user_imgs': users_imgs})
-
     return render(request, 'upload.html', {'user_imgs': users_imgs})
 
 def login_view(request):
