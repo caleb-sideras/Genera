1fde3104cb0bb4860bf2f2b9ab8d6cde8cf30f94
Caleb Sideras <CJSideras@student.fullsail.edu>
2021-11-08 17:37:58 -0500

    typing up code

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 from PIL import Image
 import numpy as np
-
-# import asset_index
-# from random_number import generateRandomNumber
 import random
 
 # Notes
-# - the current textures Samoshin sent me are buggy, they only use A (Alpha) values in the RGBA format. He will fix
-# - good luck 🙂 Caleb in yr13 would have no idea how this works!!!
-
+# - the current textures Samoshin sent me are buggy, they only use A (Alpha) values in the RGBA format
 
 # TODO
 #
@@ -18,45 +13,15 @@ import random
 #   https://docs.opensea.io/docs/metadata-standards
 #   https://docs.opensea.io/docs/2-adding-metadata
 #
-# 2) full website integration
+# 2) Full website integration
+#   Update json format / iteration to fit artems json
 #
-#   - Using rarity attribute do determine which assets/textures are selected
-#       1) Convert each layer/texture rarity section into an array of numbers corresponding to it rarity
-#           E.g. RarityA: 3 |
-#                RarityB: 2 |=> Array[10] = {A,A,A,B,B,C,C,C,C,C}, then random.choice()
-#                RarityC: 5 |
-#
-#       2) Collection Size Method: (collection size * (asset rarity *asset))
-#           E.g. Layer 1 Assets = (100/10 *(3A, 2B, 5C)) = (30A, 20B, 50C) = 100 total assets.
-#           Then you would random.choice(), subtract an element, and do this until its empty
-
-#       3) Collection Size Method with texture rarity: (collection size * (texture rarity *asset))
-#           E.g. Layer 1 Assets = (100/10 * (3A, 2B, 5C)) = (30A, 20B, 50C) = 100 total assets.
-#           E.g. Layer 1 Textures = (100/10 * (3A, 2B, 5C)) = (30A, 20B, 50C) = 100 total textures.
-#           random.choice both arrays, and subtract both elements.
-#           => (29A, 20B, 50C) = 99 total assets
-#           => (30A, 20B, 49C) = 99 total assets
-#
-# head
-# 1 20 -> 2
-# 2 20 -> 2
-# 3 60 -> 6
-
-# body			collection size 10
-# 1 50 -> 5
-# 2 50 -> 5
-
-# face
-# 1 5 ? // if we pick one, will show asset rarity 10 % on opensea
-# 2 5 ?
-# 3 90 -> 9
-
 # ---DONE---
 #
 #   - the texture mapping process can be put into a function so any asset can have a texture put on it (maybe you can do this: DD)
 #   - an example dictionary that would store all the collection data, that would be passed from the website
 #   - assembling image and textures based on this example dictionary
-#
+#   - tat added deterministic probability
 
 
 def textureMapping(asset, texture):
@@ -96,6 +61,13 @@ def textureMapping(asset, texture):
 
 def metadatacreation():
     print(temp)
+
+
+def rarityAppend(json_object, json_name, rarity_list):
+    for asset in json_object[json_name]:
+        rarity = asset["Rarity"]
+        for x in range(rarity):
+            rarity_list.append(asset["PIL"])
 
 
 # Example assets
@@ -173,14 +145,6 @@ rarityArrayTexture = []
 texturedAssetArray = []
 texturedAssetDict = {}
 
-
-def rarityAppend(json_object, json_name, rarity_list):
-    for asset in json_object[json_name]:
-        rarity = asset["Rarity"]
-        for x in range(rarity):
-            rarity_list.append(asset["PIL"])
-
-
 for var in tempDict["Layers"]:
     chosenAsset = 0
     texturedAsset = 0
@@ -245,33 +209,3 @@ for i in range(tempDict["Collection Size"]):
         im.paste(temp_asset, (0, 0), temp_asset)
 
     im.save(f"new{i}.png", "PNG")
-
-
-# then can save this image like: im.save("new.png", "PNG") or save to an array
-
-
-# print(texturedAssetArray)
-
-#     print(rarityArrayAsset)
-
-# im.paste(chosenAsset['PIL'], (0, 0), chosenAsset['PIL'])
-
-# im.paste(texturedAsset, (0, 0), texturedAsset)
-
-
-# Looping through json
-# creating both arrays (texture and asset) of 10
-# random.choice/removing them
-# texureMapping function()
-# store these new assets in another array
-# once iterated over all json, use these array to combine into final 10 images
-# store these images in an array and/or save them an .png
-
-
-# temp = random.choice(rarityArrayAsset)
-# print(rarityArrayAsset)
-# rarityArrayAsset.remove(temp)
-# print(rarityArrayAsset)
-
-# saving
-# im.save("new.png", "PNG")
