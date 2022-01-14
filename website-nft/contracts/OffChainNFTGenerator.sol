fe8031d9f23b4c78b6f2be428ff7900314ecdbe6
Caleb Sideras <CJSideras@student.fullsail.edu>
2022-01-14 14:57:50 -0500

    Contract ownership inheritance

--- website-nft/contracts/OffChainNFTGenerator.sol
+++ website-nft/contracts/OffChainNFTGenerator.sol
@@ -3,15 +3,10 @@
 pragma solidity 0.6.6;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract NFTGenerator is ERC721 {
+contract NFTGenerator is ERC721, Ownable {
     uint256 public tokenCounter;
-
-    // mapping(uint256 => Breed) public tokenIdToBreed; // every TokenId has a breed, saves all tokenIds?
-    // mapping(bytes32 => address) public requestIdToSender;
-    // event requestedCollectible(bytes32 indexed requestId, address requester); // indexed faster searching, + good practice to emit an event mapped data after changing a mapping... also helps for testing
-
-    // event breedAssigned(uint256 indexed tokenId, Breed breed);
 
     constructor(string memory _name, string memory _symbol)
         public
@@ -20,12 +15,8 @@ contract NFTGenerator is ERC721 {
         tokenCounter = 0;
     }
 
-    function createNewCollectible(string memory _tokenURI) public {
+    function createNewNFT(string memory _tokenURI) public onlyOwner {
         uint256 newTokenId = tokenCounter;
-        // require(
-        //     _isApprovedOrOwner(_msgSender(), newTokenId),
-        //     "ERC721: caller is not owner nor approved"
-        // ); //only the owner of contract can change tokenURI?
         _safeMint(msg.sender, newTokenId); // openzeppelin safe mint
         _setTokenURI(newTokenId, _tokenURI); // openzeppelin set token uri
         tokenCounter = tokenCounter + 1;
--- website-nft/scripts/deploy_and_create.py
+++ website-nft/scripts/deploy_and_create.py
@@ -1,6 +1,6 @@
 from scripts.helpful_scripts import get_accounts, OPENSEA_URL
 
-from brownie import NFTGenerator
+from brownie import NFTGenerator, Contract, accounts
 
 sample_metadata = (
     {
@@ -15,12 +15,13 @@ sample_uri = "ipfs://bafybeiha4htobdjlk5vyusnjksxk5bxoc3oq746uxofpvnt2ua6vh7pvhe
 
 
 def deploy_and_create():
-    account = get_accounts()
-    collectible = NFTGenerator.deploy("VoidChan", "vde", {"from": account})
-    creating_tx = collectible.createNewCollectible(sample_uri, {"from": account})
+    # account = get_accounts()
+    # collectible = NFTGenerator.deploy("VoidChan", "vde", {"from": account})
+    contract = Contract("0x47CE21A2f6fd72D3e2a94277EA1b74B332E60189")
+    creating_tx = contract.createNewNFT(sample_uri, {"from":  accounts.add('0dc39a3e4a7e928ac749bcbf4ce8ac827dac3df543633820ac1b05a5801a979f')})
     creating_tx.wait(1)
     print("New token has been created!")
-    return collectible, creating_tx
+    return contract, creating_tx
 
 
 def get_OPENSEA_URL(nft_contract):
