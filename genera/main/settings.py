f3ebe4ee81dc890fc5e458b26a7de441b535700d
Artem Grechushkin <artem.grechushkin@mail.ru>
2022-01-28 04:38:34 +0000

    test

--- genera/main/settings.py
+++ genera/main/settings.py
@@ -52,7 +52,7 @@ if DEBUG:
     import mimetypes
     mimetypes.add_type("application/javascript", ".js", True)
 
-ALLOWED_HOSTS = ['3.14.255.107', 'localhost', '3.14.255.107:8000']
+ALLOWED_HOSTS = ['3.14.255.107', 'localhost', '3.14.255.107:8000', '0.0.0.0']
 
 # Application definition
 APPS = ['main', 'payments']
