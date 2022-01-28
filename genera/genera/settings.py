4fd1525106101baf2343df0b5958b28edfce1a8d
Artem Grechushkin <artem.grechushkin@mail.ru>
2022-01-28 05:03:25 +0000

    remove idito settings

--- genera/genera/settings.py
+++ genera/genera/settings.py
@@ -52,7 +52,7 @@ if DEBUG:
     import mimetypes
     mimetypes.add_type("application/javascript", ".js", True)
 
-ALLOWED_HOSTS = ['localhost','3.145.108.169']
+ALLOWED_HOSTS = ['localhost','3.14.255.107']
 
 # Application definition
 APPS = ['main', 'payments']
