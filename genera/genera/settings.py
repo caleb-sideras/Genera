4169ac12bdaf34f64b0036b5ab848b874f165ab8
Artem Grechushkin <artem.grechushkin@mail.ru>
2022-02-02 07:58:19 +0000

    test ssh

--- genera/genera/settings.py
+++ genera/genera/settings.py
@@ -100,6 +100,17 @@ WSGI_APPLICATION = 'genera.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': "genera",#os.environ['RDS_USERNAME']
+            'PASSWORD': "'g'9F~;<xkmr)~^Z",
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
+
 if 'RDS_DB_NAME' in os.environ:
     DATABASES = {
         'default': {
@@ -111,13 +122,13 @@ if 'RDS_DB_NAME' in os.environ:
             'PORT': os.environ['RDS_PORT'],
         }
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
 
 
 # Password validation
