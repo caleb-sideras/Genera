1525c5a5137949b0e0e26ab20197902d65912724
Artem Grechushkin <artem.grechushkin@mail.ru>
2022-02-04 16:12:56 +0000

    xd reset

--- genera/genera/settings.py
+++ genera/genera/settings.py
@@ -100,16 +100,16 @@ WSGI_APPLICATION = 'genera.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-# DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.mysql',
-#             'NAME': "ebdb",
-#             'USER': "genera",#os.environ['RDS_USERNAME']
-#             'PASSWORD': "'g'9F~;<xkmr)~^Z",
-#             'HOST': "aa1pitge69y69xv.cb2fcrzehayo.us-east-2.rds.amazonaws.com",
-#             'PORT': "3306",
-#         }
-#     }
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': "ebdb",
+            'USER': "genera",#os.environ['RDS_USERNAME']
+            'PASSWORD': "'g'9F~;<xkmr)~^Z",
+            'HOST': "aa1pitge69y69xv.cb2fcrzehayo.us-east-2.rds.amazonaws.com",
+            'PORT': "3306",
+        }
+    }
 
 if 'RDS_DB_NAME' in os.environ:
     DATABASES = {
@@ -122,13 +122,13 @@ if 'RDS_DB_NAME' in os.environ:
             'PORT': os.environ['RDS_PORT'],
         }
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
 
 
 # Password validation
