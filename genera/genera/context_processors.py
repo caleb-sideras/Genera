8e7b58524e541b3f72edaf984dead6135aee4d60
Artem Grechushkin <artem.grechushkin@mail.ru>
2022-02-06 20:13:22 +0000

    close

--- genera/genera/context_processors.py
+++ genera/genera/context_processors.py
@@ -2,17 +2,17 @@
 def protecc(request):
     try:
         request.session["enter_page"]
-        request.session["attempts"]
+        # request.session["attempts"]
     except:
         request.session["enter_page"] = False
-        request.session["attempts"] = 0
+        # request.session["attempts"] = 1
 
-    if request.session["attemps"] < 5:
-        if request.method == 'POST':
-            code = request.POST.get("code", "")
-            if code == "577677":
-                request.session["enter_page"] = True
-            else:
-                request.session["attempts"] += 1
+    # if request.session["attemps"] < 5:
+    if request.method == 'POST':
+        code = request.POST.get("code", "")
+        if code == "577677":
+            request.session["enter_page"] = True
+        # else:
+        #     request.session["attempts"] += 1
     
     return {}
