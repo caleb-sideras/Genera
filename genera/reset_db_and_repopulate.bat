f0ae81f6cf15b3ef8ccd0582f918c0ecaa582f16
Artem Grechushkin <artem.grechushkin@mail.ru>
2022-02-08 17:47:57 +0000

    db script to be relevant to deployment cossplay

--- genera/reset_db_and_repopulate.bat
+++ genera/reset_db_and_repopulate.bat
@@ -1,7 +1,4 @@
-call DEL /S /Q "db.sqlite3"
-call RMDIR "media" /S /Q
 call python manage.py makemigrations
-call python manage.py migrate
 call python manage.py migrate --run-syncdb
 call python populate.py
 call PAUSE
