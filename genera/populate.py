52d2cb8e7145dedfb03f7408f9fd21eb141e9ca7
Artem Grechushkin <artem.grechushkin@mail.ru>
2022-02-10 20:47:26 +0000

    sd

--- genera/populate.py
+++ genera/populate.py
@@ -21,7 +21,7 @@ def populate():
     else:
         print("USER ALREADY EXISTS - MAKE SURE TO WIPE DB!!!!")
     if not User.objects.filter(username="caleb").exists():
-        UserProfile.objects.create(user=User.objects.create_superuser(username="caleb", password="1234", credits=1000x))
+        UserProfile.objects.create(user=User.objects.create_superuser(username="caleb", password="1234", credits=1000))
     
 
 
