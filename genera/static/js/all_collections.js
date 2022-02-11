4fbaa1b6e19479b087c66b9737efb47dcea79538
Artem Grechushkin <artem.grechushkin@mail.ru>
2022-02-11 14:16:47 +0000

    more concepts

--- genera/static/js/all_collections.js
+++ genera/static/js/all_collections.js
@@ -1,9 +1,9 @@
-const check_collection_status = (url) => {
+const check_collection_status = (url, collection_name) => {
     fetch(url).then(response => {
         return response.json();
     }).then(data => {
         if (data["complete"]){
-            create_notification("Generation Complete!", "Your collection has been generated! Page will be refreshed automatically :)", duration=5000, theme="success")
+            create_notification("Generation Complete!", `Your collection '${collection_name}' has been generated! Thank you for your patience - page will be refreshed automatically :)`, duration=5000, theme="success")
             setTimeout(function () {
                 location.reload()
             }, 5000);
