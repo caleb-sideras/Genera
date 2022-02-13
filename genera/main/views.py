1ff03852f8eece1f236f7abd8e61eb388ae96e8d
Artem Grechushkin <artem.grechushkin@mail.ru>
2022-02-13 21:28:28 +0000

    printer

--- genera/main/views.py
+++ genera/main/views.py
@@ -270,9 +270,7 @@ def upload_view(request):
                 user.credits -= db_collection.collection_size
                 user.save()
 
-                print(db_collection.collection_size)
                 if db_collection.collection_size > 20: #put on a thread if > 20, else we can handle normally
-                    print("COLLECTION SIZE MASSIVE")
                     if create_and_save_collection_paid_thread(calebs_gay_dict, db_collection, required_dicts[0], required_dicts[1], request.user): #checks if thread has started! if so - redirect to all collections page!
                         messages.success(request, message="Your collection is quite large and is being generated. You've been redirected to your collections page! Thank you for your patience.")
                         return ajax_redirect(reverse("main:all_collections", args=[request.user.username_slug]))
