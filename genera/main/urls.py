2fb98a6620c009af870ebc68b2763771cdf0b6c4
Artem Grechushkin <artem.grechushkin@mail.ru>
2022-03-03 21:57:33 +0000

    urls

--- genera/main/urls.py
+++ genera/main/urls.py
@@ -10,41 +10,41 @@ app_name = "main"
 urlpatterns = [
     # include other apps
     path("", home_view, name="home"),
-    path("learn", main_view, name="main_view"),
-    path("make", upload_view, name="upload"),
+    path("learn/", main_view, name="main_view"),
+    path("make/", upload_view, name="upload"),
 
-    path("login/options", login_options_view, name="login_options"),
+    path("login/options/", login_options_view, name="login_options"),
 
-    path("login/regular", login_view, name="login"),
+    path("login/regular/", login_view, name="login"),
     
-    path("login/metamask", metamask_login_handler_view, name="login_metamask_handler"), #no template for this view - just a url to handle the metamask login stuff
+    path("login/metamask/", metamask_login_handler_view, name="login_metamask_handler"), #no template for this view - just a url to handle the metamask login stuff
 
-    path("logout", logout_view, name="logout"),
-    path("logout/<path:current_extension>", logout_view, name="logout"),
+    path("logout/", logout_view, name="logout"),
+    path("logout/<path:current_extension>/", logout_view, name="logout"),
 
-    path("register", register_view, name="register"),
-    path("register/<uuid:token_url>", account_activation_view, name="account_activation"),
+    path("register/", register_view, name="register"),
+    path("register/<uuid:token_url>/", account_activation_view, name="account_activation"),
 
-    path("reset", password_reset_view, name="password_reset"),
-    path("reset/<uuid:token_url>", password_reset_handler_view, name="password_reset_handler"),
+    path("reset/", password_reset_view, name="password_reset"),
+    path("reset/<uuid:token_url>/", password_reset_handler_view, name="password_reset_handler"),
 
-    path("about", about_view, name="about"),
-    path("docs", documentation_view, name="documentation"),
+    path("about/", about_view, name="about"),
+    path("docs/", documentation_view, name="documentation"),
 
-    path("search=<slug:search_query>", search_view, name="search_view"),
+    path("search=<slug:search_query>/", search_view, name="search_view"),
 
-    path("@<slug:username_slug>", profile_view, name="profile"),
-    path("@<slug:username_slug>/<str:contract_address>", mint_view, name="user_mint"),
-    path("@<slug:username_slug>/collections/all", all_collections_view, name="all_collections"),
-    path("@<slug:username_slug>/collections/<slug:collection_name_slug>", collection_view, name="collection"),
-    path("@<slug:username_slug>/collections/<str:collection_name_slugs>/request_status", collection_view_loaded_handler, name="collection_loaded_handler"),
+    path("@<slug:username_slug>/", profile_view, name="profile"),
+    path("@<slug:username_slug>/<str:contract_address>/", mint_view, name="user_mint"),
+    path("@<slug:username_slug>/collections/all/", all_collections_view, name="all_collections"),
+    path("@<slug:username_slug>/collections/<slug:collection_name_slug>/", collection_view, name="collection"),
+    path("@<slug:username_slug>/collections/<str:collection_name_slugs>/request_status/", collection_view_loaded_handler, name="collection_loaded_handler"),
     # path("create-checkout-session", checkout_session, name="create-checkout-session")
 
-    path("mint", public_mint_view, name="mint"),
-    path("privacy", policy_view, name="privacy"),
-    path("terms", terms_view, name="terms"),
-    path("contact_us/problem_report", problem_report_view, name="problem_report"),
-    path("dashboard/reported_issues", reported_issues_view, name="reported_issues"),
+    path("mint/", public_mint_view, name="mint"),
+    path("privacy/", policy_view, name="privacy"),
+    path("terms/", terms_view, name="terms"),
+    path("contact_us/problem_report/", problem_report_view, name="problem_report"),
+    path("dashboard/reported_issues/", reported_issues_view, name="reported_issues"),
 
     path("robots.txt", robots_txt_view, name="robots"),
 ]
